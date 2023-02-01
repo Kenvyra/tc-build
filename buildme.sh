70700461bdc3011c0b502fb744bf1dd6f35302f3
Jens Reidel <adrian@travitia.xyz>
2023-02-01 15:49:59 +0100

    Use Alpine Dockerfile for build script
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- buildme.sh
+++ buildme.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-podman build -t clang:latest .
+podman build -t clang:latest -f Dockerfile.alpine .
 podman run --rm -it --name clang --privileged --ulimit=host --ipc=host --cgroups=disabled --security-opt label=disable --userns=keep-id --hostname $(hostname) --mount type=tmpfs,tmpfs-size=200G,destination=/build -v $(pwd):/src clang:latest
